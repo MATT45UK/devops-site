25291fa6f8a81f48034093807da25eec5fefc055
Matt Bailey <matt.bailey@elucidat.com>
2020-05-05 11:07:32 +0100

    alter methods used for various calls

--- src/routes/terraform.routes.js
+++ src/routes/terraform.routes.js
@@ -1,13 +1,13 @@
 module.exports = (app) => {
     const terraform = require('../controllers/terraform.controller.js');
 
-    app.get('/terraform/preinit/:userId', terraform.preInitialise);
-    app.get('/terraform/init/:userId', terraform.init);
+    app.put('/terraform/preinit/:userId', terraform.preInitialise);
+    app.put('/terraform/backend/:userId', terraform.createBackend);
+    app.put('/terraform/init/:userId', terraform.init);
+
     app.get('/terraform/plan/:userId', terraform.plan);
 
-
     // Create a new product
-    app.post('/terraform/backend/:userId', terraform.createBackend);
     app.post('/terraform/provisioner/:userId', terraform.createProvisioners);
     app.post('/terraform/module/:userId', terraform.createModule);
 
