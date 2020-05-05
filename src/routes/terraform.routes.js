9cd101c8e5c931cbc526efc4fa34b7405c6c74c2
Matt Bailey <matt.bailey@elucidat.com>
2020-05-05 10:41:20 +0100

    preinit should be get

--- src/routes/terraform.routes.js
+++ src/routes/terraform.routes.js
@@ -1,12 +1,12 @@
 module.exports = (app) => {
     const terraform = require('../controllers/terraform.controller.js');
 
+    app.get('/terraform/preinit/:userId', terraform.preInitialise);
     app.get('/terraform/init/:userId', terraform.init);
     app.get('/terraform/plan/:userId', terraform.plan);
 
 
     // Create a new product
-    app.post('/terraform/preinit/:userId', terraform.preInitialise);
     app.post('/terraform/backend/:userId', terraform.createBackend);
     app.post('/terraform/provisioner/:userId', terraform.createProvisioners);
     app.post('/terraform/module/:userId', terraform.createModule);
