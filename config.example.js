e0e82273218e433e8d7fbf7eccbbcf9339a3314d
Matt Bailey <matt.bailey@elucidat.com>
2020-05-07 11:09:18 +0100

    update config example

--- config.example.js
+++ config.example.js
@@ -4,5 +4,8 @@ module.exports = {
         region: "eu-west-2",
         accessKey: "",
         secretKey: ""
+    },
+    googleAuth: {
+        secret: '' // Google OAuth 2.0 Client ID's Client secret 
     }
 }
